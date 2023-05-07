e51e99b5b7b3d2441646008e04e7dbbba0fb7d95
lengmuzhaxi123 <128277531+lengmuzhaxi123@users.noreply.github.com>
2023-05-07 22:10:39 +0800

    Update 11764.cpp

--- 11764.cpp
+++ 11764.cpp
@@ -11,7 +11,7 @@
 //			l = 0; h = 0;
 //			for (int i = 0; i < N; ++i)
 //				cin >> H[i];
-//			for (int i = 0; i < N; ++i)
+//			for (int i = 0; i < N-1; ++i)
 //			{
 //				if (H[i+1] > H[i])
 //					++h;
